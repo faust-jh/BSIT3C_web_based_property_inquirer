46e4ef2ef45ecb849e04ecbcb2e39e46a4f35e59
faust-jh <faustinojheru@gmail.com>
2022-04-18 22:08:34 +0800

    dash:
    ;
    e
    x
    q

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import Head from "next/head";
+import { AppBar, Box, IconButton, Toolbar } from "@mui/material"
+ 
 
+
+
+//EXPORT
 export default function dashboard() {
+  
+  //RETURN
   return (
-    <div>dashboard</div>
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Head>
+        <title> Dashboard </title>
+      </Head>
+      <AppBar>
+        <Toolbar>
+          <IconButton>
+            
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    
   )
 }
