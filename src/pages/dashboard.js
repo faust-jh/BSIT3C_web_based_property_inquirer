76790fd9bd8a21ab93f172ff3adef4f1e77831a6
RobertJr15 <robertjrdevera15@gmail.com>
2022-05-03 22:09:50 +0800

    Drawer for Dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,18 +1,37 @@
 import React from "react";
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import {
   AppBar,
   Box,
+  Drawer,
+  Divider,
   IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   Toolbar,
   Typography,
-  Drawer,
 } from "@mui/material";
 
 //EXPORT
 export default function dashboard() {
-  //RETURN
+  //CONSTANTS
+  const [open, setOpen] = useState(false);
+
+  //To Open the Drawer
+  const setDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  //To Close the Drawer
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  //THE DISPLAY
   return (
     <Box
       sx={{
@@ -29,7 +48,7 @@ export default function dashboard() {
       </Head>
       <AppBar>
         <Toolbar>
-          <IconButton>
+          <IconButton onClick={setDrawerOpen}>
             <Image
               src="/assets/svg/option.svg"
               alt="options"
@@ -47,7 +66,100 @@ export default function dashboard() {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer anchor={"left"} variant="permanent" open={true}></Drawer>
+      <Drawer anchor="left" open={open} onClose={handleClose}>
+        <Box sx={{ minWidth: "200px", height: "100vh", display: "flex", flexDirection: "column"}}>
+          <List sx={{ marginTop: "55px", flexGrow: 1 }}>
+
+            <Divider/>
+
+            <ListItem button>
+              <ListItemIcon>
+                <Image
+                  src="/assets/svg/dashboard.svg"
+                  alt="dashboard"
+                  width="20"
+                  height={20}
+                />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard"/>
+            </ListItem>
+
+            <Divider/>
+
+            <ListItem button>
+              <ListItemIcon>
+                <Image
+                  src="/assets/svg/deals.svg"
+                  alt="deals"
+                  width="20"
+                  height={20}
+                />
+              </ListItemIcon>
+              <ListItemText primary="Deals"/>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <Image
+                  src="/assets/svg/property.svg"
+                  alt="property"
+                  width="20"
+                  height={20}
+                />
+              </ListItemIcon>
+              <ListItemText primary="Properties"/>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <Image
+                  src="/assets/svg/loan.svg"
+                  alt="loan"
+                  width="20"
+                  height={20}
+                />
+              </ListItemIcon>
+              <ListItemText primary="Loans"/>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <Image
+                  src="/assets/svg/document.svg"
+                  alt="document"
+                  width="20"
+                  height={20}
+                />
+              </ListItemIcon>
+              <ListItemText primary="Documents"/>
+            </ListItem>
+
+            <Divider/>
+
+            <ListItem button>
+              <ListItemIcon>
+                <Image
+                  src="/assets/svg/settings.svg"
+                  alt="settings"
+                  width="20"
+                  height={20}
+                />
+              </ListItemIcon>
+              <ListItemText primary="Settings"/>
+            </ListItem>
+          </List>
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <Image
+                  src="/assets/svg/signout.svg"
+                  alt="signout"
+                  width="20"
+                  height={20}
+                />
+              </ListItemIcon>
+              <ListItemText primary="Sign Out"/>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
     </Box>
   );
 }
