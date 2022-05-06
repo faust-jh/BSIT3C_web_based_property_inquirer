49d62bed470aa071ce7ac969cfd3b3ca018c7bd8
faust-jh <faustinojheru@gmail.com>
2022-05-06 20:27:02 +0800

    images and index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import { AppBar, Toolbar } from "@mui/material";
+import { Box } from "@mui/system";
+import Head from "next/head";
+import React from "react";
+import design from "../styles/designs";
 
 export default function index() {
   return (
-    <div>index</div>
-  )
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Head>
+        <title> Yokuso! </title>
+      </Head>
+      <AppBar
+        sx={{
+          height: "11vh",
+          bgcolor: "#E8F9FD",
+        }}
+      >
+        <Toolbar></Toolbar>
+      </AppBar>
+
+      <img src="/images/1.jpg" alt="qwe" sx={{ ...design.img1 }} />
+    </Box>
+  );
 }
--- src/styles/designs.js
+++ src/styles/designs.js
@@ -1,10 +1,13 @@
 const design = {
-
   button1: {
     borderRadius: "100px",
     textTransform: "none",
   },
-  
+
+  img1: {
+    outerHeight: "100px",
+    outerWidth: "100px",
+  },
 };
 
 export default design;
