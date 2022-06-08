91d3eed4ac37a295f473a556e10fff7775f41f5b
faust-jh <faustinojheru@gmail.com>
2022-06-08 21:32:05 +0800

    layout

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -2,7 +2,7 @@ import React from "react";
 import Admin from "../components/Admin";
 
 export default function Settings() {
-  return <div>Settings</div>;
+  return <div>Settingsqqqq</div>;
 }
 
 Settings.getLayout = function getLayout(page) {
